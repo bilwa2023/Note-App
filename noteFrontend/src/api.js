b512174d779169af63d11df441690807bcec7229
Bilwamangal Biswal <168819444+bilwa2023@users.noreply.github.com>
2025-09-05 21:16:03 +0530

    Update api.js

--- noteFrontend/src/api.js
+++ noteFrontend/src/api.js
@@ -6,3 +6,4 @@ export const getNotes = () => axios.get(`${API}/notes`);
 export const createNote = (note) => axios.post(`${API}/notes`, note);
 export const updateNote = (id, note) => axios.put(`${API}/notes/${id}`, note);
 export const deleteNote = (id) => axios.delete(`${API}/notes/${id}`);
+//fetch api
