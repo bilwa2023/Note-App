0e32eccf0781a386e9f4a69e8aefc90de8785bb8
Bilwamangal Biswal <168819444+bilwa2023@users.noreply.github.com>
2025-09-05 16:57:19 +0530

    Update api.js

--- noteFrontend/src/api.js
+++ noteFrontend/src/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = import.meta.env.VITE_API_URL || "note-app-production-5d2b.up.railway.app";
+const API = import.meta.env.VITE_API_URL || "https://note-app-production-5d2b.up.railway.app";
 
 export const getNotes = () => axios.get(`${API}/notes`);
 export const createNote = (note) => axios.post(`${API}/notes`, note);
